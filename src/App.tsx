29a5bb5906a523ae53e42f95a20d8422e70365b1
Eli Dresnin <s018305@students.lmsd.org>
2024-02-09 10:39:24 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,85 @@
+import MyCrossword from 'mycrossword';
+import 'mycrossword/dist/index.css';
+import * as React from 'react';
+import './App.css';
+import data from './guardian.cryptic.28505';
+
+// TODO: import from MyCrossword
+const themeColors = [
+  'red',
+  'pink',
+  'purple',
+  'deepPurple',
+  'indigo',
+  'blue',
+  'lightBlue',
+  'cyan',
+  'teal',
+  'green',
+  'deepOrange',
+  'blueGrey',
+] as const;
+type Theme = typeof themeColors[number];
+
+export default function App() {
+  const allowedHtmlTags = ['b', 'strong', 'i', 'em', 'sub', 'sup'];
+  const [theme, setTheme] = React.useState<Theme>('blue');
+  const [showDefinitions, setShowDefinitions] = React.useState(false);
+
+  return (
+    <>
+      <header>
+        <span className="logo">MyCrossword</span>
+        <span className="example">Example</span>
+      </header>
+      <main>
+        <div className="crossword-title-container">
+          <h1 className="crossword-title">{data.name}</h1>
+          <span className="crossword-subtitle">
+            {new Date(data.date).toLocaleDateString('en-GB', {
+              day: 'numeric',
+              month: 'short',
+              year: 'numeric',
+            })}
+            {data.creator !== undefined
+              ? `, set by ${data.creator.name}`
+              : null}
+          </span>
+        </div>
+        <div className="toolbar">
+          <div className="theme-selector">
+            <label htmlFor="theme">Theme</label>
+            <select
+              id="theme"
+              value={theme}
+              onChange={(e) => setTheme(e.target.value as Theme)}
+            >
+              {themeColors.map((color) => (
+                <option key={color} value={color}>
+                  {color}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="show-definitions-container">
+            <input
+              checked={showDefinitions}
+              id="show-definitions"
+              onClick={() => setShowDefinitions(!showDefinitions)}
+              type="checkbox"
+            />
+            <label htmlFor="show-definitions">Show definitions</label>
+          </div>
+        </div>
+        <MyCrossword
+          allowedHtmlTags={
+            showDefinitions ? [...allowedHtmlTags, 'u'] : allowedHtmlTags
+          }
+          id={data.id}
+          data={data}
+          theme={theme}
+        />
+      </main>
+    </>
+  );
+}
